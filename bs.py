1be73d5230f2921c21b608edafdc990a81ed2c44
running911 <running911@msn.com>
2016-11-21 21:04:36 +0800

    added abc.txt\byColor.py\os.py and modified bs.py

--- bs.py
+++ bs.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-html=urlopen("http://pythonscraping.com/pages/page1.html")
-bsObj=BeautifulSoup(html.read(),'lxml')
-print(bsObj)
-print(bsObj.h1)
-print(bsObj.html.body.h1)
-print(bsObj.body.h1)
-print(bsObj.html.h1)
+from urllib.error import HTTPError
+def getTitle(url):
+	try:
+		html=urlopen(url)
+	except HTTPError as e:
+		return None
+	try:
+		bsObj=BeautifulSoup(html.read(),'lxml')
+		title=bsObj.body.h1
+	except AttributerError as e:
+		return None
+	return title
+	
+title=getTitle("http://pythonscraping.com/pages/page1.html")
+if title==None:
+	print("title could not be found")
+else:
+	print(title)
+
+
 
---
+++ byColor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html=urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+bsObj=BeautifulSoup(html.read(),"lxml")
+#nameList=bsObj.findAll("span",{"class":"green"})
+#for name in nameList:
+#	print(name.get_text())
+
+print(bsObj)
+hx=bsObj.findAll("h1")
+print(hx)
